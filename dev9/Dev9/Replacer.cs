8190af5d108cd1f9c00d691158a9f6f3db1bb46c
AndreiBakunovich <bakunovich.andrei@mail.ru>
2017-09-29 19:48:32 +0300

    Programfinished with this file.

---
+++ dev9/Dev9/Replacer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev9
+{
+    class Replacer
+    {
+        public string ReplacingPartsInStrings(char[] recipient, char[] donor)
+        {
+            string res = String.Empty;
+            //dividion lines on parts
+            Random rnd = new Random();
+
+            int donorIntervalStart = rnd.Next(0, donor.Length),
+                donorIntervalFinish = rnd.Next(donorIntervalStart, donor.Length),
+                recipientIntervalStart = rnd.Next(0, recipient.Length),
+                recipientIntervalFinish = rnd.Next(recipientIntervalStart, recipient.Length);
+            
+            //result configuring
+            for (int i = 0; i < recipientIntervalStart; i++)
+                res = res + recipient[i];
+
+            for (int i = donorIntervalStart; i < donorIntervalFinish; i++)
+                res = res + donor[i];
+
+            for (int i = recipientIntervalFinish; i < recipient.Length; i++)
+                res = res + recipient[i];
+
+            return res;
+        }
+    }
+}
