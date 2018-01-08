11273c213419412b0041f17fa1ed94610b03e2eb
AndreiBakunovich <bakunovich.andrei@mail.ru>
2018-01-08 03:12:44 +0300

    Path validation to folder or file in windows OS.

---
+++ Exam1_TAT_RFiKT_2017_1/ControleWork1/ControlWork1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_01
+{
+    class Program
+    {
+        // this program check is path valid
+        static void Main ( string [] args )
+        {
+            bool possiblePath = true;
+            try
+            {
+                Checker MyChecker = new Checker();
+                possiblePath = MyChecker.Check(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine ( "Wrong source file format or incorrect path to them!" );
+                return;
+            }
+            Console.WriteLine ( "Path is {0}." , possiblePath );
+        }
+    }
+}
